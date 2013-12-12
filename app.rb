ea049a3b1bbb3de63cb02f7cfafb7b6223ae789c
Kphillycat <keithwilliamsjr@gmail.com>
2013-12-12 08:26:17 -0500

    Add uri encoding in app.rb for more accurate searching with spotify search api

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ module Playlist
 		get '/song/:name' do
 			@playlister = Parser.new("./data")
 			@song = @playlister.find_song(params[:name])
-			@download = open("http://ws.spotify.com/search/1/track.json?q=#{@song.name}").read
+			@download = open("http://ws.spotify.com/search/1/track.json?q=#{URI::encode(@song.name)}").read
 			@html = JSON.parse(@download)
 			@spotify_url = @html["tracks"][0]["href"]
 			erb :song
