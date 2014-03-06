9ae5e4fe91f29ab285d15314946e712ee90af5ae
Kphillycat <keithwilliamsjr@gmail.com>
2014-03-06 11:38:23 -0500

    Add messages for no news

--- public/js/echonest.js
+++ public/js/echonest.js
@@ -7,38 +7,17 @@ $(document).ready(function(){
             api_key: '1HBIPJWJYKGSJSTEF',
             name: artist,
             results: 5,
-            // xhrFields: { withCredentials: true},
             high_relevance: true, 
     }; 
 
   $.get(url, args, function(data) {
-                      console.log("makin' request");
-                      $.each(data.response.news, function(index, news) {
-                        var div = formatNews(news);
-                        $(".artist-news").append(div);
-                      });
-                      // $(".artist-news").
-                    //     $(".artist-news").empty();
-                    //     if (! ('news' in data.response)) {
-                    //         console.log("error")
-                    //         error("Can't find any news for " + artist);
-                    //     } else {
-                    //         console.log(data);
-                    //         $(".artist-news").show();
-                    //         var titles = {}
-                    //         $.each(data.response.news, function(index, news) {
-                    //             if (! (news.name in titles)) {
-                    //                 var div = formatNews(news);
-                    //                 $(".artist-news").append(div);
-                    //                 titles[news.name] = 1;
-                    //             }
-                    //         });
-                    //   info("Showing " + data.response.news.length + " of " + data.response.total + " news items for " + artist);
-                    // }
-                    // },
-                    // function() {
-                    //     error("Trouble getting news for " + artist);
-                    }
+                    console.log("makin' request");
+                    if (data.response.news.length == 0) {$(".artist-news").append("<h2>Sorry no news today!</h2>");}
+                    $.each(data.response.news, function(index, news) {
+                      var div = formatNews(news);
+                      $(".artist-news").append(div);
+                    });
+                  }
   );
 
   function formatNews(news) {
